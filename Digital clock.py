6593f0f27124f484abbed0c06099b858cadb1c0d
Pranav Pakalapati <117028194+PranavPakalapati@users.noreply.github.com>
2024-09-23 10:14:34 +0530

    Update Digital clock.py

--- Digital clock.py
+++ Digital clock.py
@@ -12,7 +12,7 @@ def get_time():
     timeVar = time.strftime("%I:%M:%S %p")
     clock.config(text=timeVar)
     clock.after(200,get_time)
-
+    
 
 Label(master,font=("Arial",30),text="Digital Clock",fg="white",bg="black").pack()
 clock = Label(master, font=("Arial",100),bg="black",fg="white")
